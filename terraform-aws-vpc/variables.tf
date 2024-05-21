181938d937a8e2468a2e19f5f7c9b2db3ac766ca
chandureddy08 <chandrareddyerigela@gmail.com>
2024-05-21 15:42:31 -0400

    terraform

--- terraform-aws-vpc/variables.tf
+++ terraform-aws-vpc/variables.tf
@@ -36,11 +36,79 @@ variable "igw_tags" {
 variable "public_subnet_cidrs" {
     type = list
     validation {
-      condition = length(var.public_subnet_cidrs ) == 2
+      condition = length(var.public_subnet_cidrs) == 2
       error_message = "Please provide 2 valid subnet CIDR"
     }
 }
 variable "public_subnet_cidrs_tags" {
+    type = map
+    default = {}
+}
+
+## PRIVATE SUBNET ##
+variable "private_subnet_cidrs" {
+  type = list
+  validation {
+    condition = length(var.private_subnet_cidrs)  == 2
+    error_message = "Please provide 2 valid subnet CIDR"
+  }
+}
+variable "private_subnet_cidrs_tags" {
+    type = map
+    default = {}
+}
+
+## DATABASE VARS ##
+variable "database_subnet_cidrs" {
+    type = list
+    validation {
+    condition = length(var.database_subnet_cidrs) == 2
+    error_message = "Please provide 2 valid subnet CIDR"
+  }
+}
+variable "database_subnet_cidrs_tags" {
+    type = map
+    default = {}
+}  
+
+## NAT GATEWAY ##
+variable "nat_gateway_tags" {
+    type = map
+    default = {}
+}
+
+## PUBLIC ROUTE TABLE VARS ##
+variable "public_route_table_tags" {
+    type = map
+    default = {}
+}
+
+## PRIVATE ROUTE TABLE VARS ##
+variable "private_route_table_tags" {
+    type = map
+    default = {}
+}
+
+## DATABASE ROUTE TABLE TAGS ##
+variable "database_route_table_tags" {
+    type = map
+    default = {}
+}
+
+## PEERING ##
+variable "is_peering_required" {
+    type = bool
+    default = false
+}
+variable "acceptor_vpc_id" {
+    type = string
+    default = ""
+}
+variable "vpc_peering_tags" {
+    type = map
+    default = {}
+}
+variable "aws_db_subnet_group_tags" {
     type = map
     default = {}
 }
--- terraform-aws-vpc/vpc.tf
+++ terraform-aws-vpc/vpc.tf
@@ -36,7 +36,153 @@ resource "aws_subnet" "public" {
         var.common_tags,
         var.public_subnet_cidrs_tags,
         {
-            Name = "${local.resource_name}-${local.az_names[count.index]}"
+            Name = "${local.resource_name}-public-${local.az_names[count.index]}"
         }
     )
-} 
+}
+
+## PRIVATE SUBNET ##
+resource "aws_subnet" "private" {
+    count = length(var.private_subnet_cidrs)
+    vpc_id = aws_vpc.main.id
+    availability_zone = local.az_names[count.index]
+    cidr_block = var.private_subnet_cidrs[count.index]
+
+    tags = merge(
+        var.common_tags,
+        var.private_subnet_cidrs_tags,
+        {
+            Name = "${local.resource_name}-private-${local.az_names[count.index]}"
+        }
+    )
+}
+
+## DATABASE SUBNET ##
+resource "aws_subnet" "database" {
+    count = length(var.database_subnet_cidrs)
+    vpc_id = aws_vpc.main.id
+    availability_zone = local.az_names[count.index]
+    cidr_block = var.database_subnet_cidrs[count.index]
+    tags = merge(
+        var.common_tags,
+        var.database_subnet_cidrs_tags,
+        {
+            Name = "${local.resource_name}-database-${local.az_names[count.index]}"
+        }
+    )
+}
+
+## DATABASE SUBNET GROUP ##
+resource "aws_db_subnet_group" "default" {
+    name = local.resource_name
+    subnet_ids = aws_subnet.database[*].id
+
+    tags = merge(
+        var.common_tags,
+        var.aws_db_subnet_group_tags,
+        {
+            Name = local.resource_name
+        }
+    )
+}
+
+## ELASTIC IP ##
+resource "aws_eip" "nat" {
+    domain = "vpc"
+}
+
+## NAT GATEWAY ##
+resource "aws_nat_gateway" "nat" {
+    allocation_id = aws_eip.nat.id
+    subnet_id = aws_subnet.public[0].id
+    
+    tags = merge(
+        var.common_tags,
+        var.nat_gateway_tags,
+        {
+            Name = local.resource_name
+        }
+    )
+    depends_on = [ aws_internet_gateway.expence ]  # explicit dependency means first create IGW and then NAT
+}
+
+## PUBLIC ROUTE TABLE ##
+resource "aws_route_table" "public" {
+    vpc_id = aws_vpc.main.id
+    
+    tags = merge(
+        var.common_tags,
+        var.public_route_table_tags,
+        {
+            Name = "${local.resource_name}-public"
+        }
+    )
+}
+
+## PRIVATE ROUTE TABLE ##
+resource "aws_route_table" "private" {
+    vpc_id = aws_vpc.main.id
+
+    tags = merge(
+        var.common_tags,
+        var.private_route_table_tags,
+        {
+            Name = "${local.resource_name}-private"
+        }
+    )
+}
+
+## DATABASE ROUTE TABLE ##
+resource "aws_route_table" "database" {
+    vpc_id = aws_vpc.main.owner_id
+
+    tags = merge(
+        var.common_tags,
+        var.database_route_table_tags,
+        {
+            Name = "${local.resource_name}-database"
+        }
+    )
+}
+
+## PUBLIC ROUTE ##
+resource "aws_route" "public_route" {
+    route_table_id = aws_route_table.public.id
+    destination_cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.expence.id
+}
+
+## PRIVATE ROUTE ##
+resource "aws_route" "private_route_nat" {
+    route_table_id = aws_route_table.private.id
+    destination_cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+}
+
+## DATABASE ROUTE ##
+resource "aws_route" "database_route_nat" {
+    route_table_id = aws_route_table.database.id
+    destination_cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+}
+
+## PUBLIC ASSOCIATION ##
+resource "aws_route_table_association" "public" {
+    count = length(var.public_subnet_cidrs)
+    subnet_id = element(aws_subnet.public[*].id, count.index)
+    route_table_id = aws_route_table.public.id
+}
+
+## PRIVATE ASSOCIATION ##
+resource "aws_route_table_association" "private" {
+    count = length(var.private_subnet_cidrs)
+    subnet_id = element(aws_subnet.private[*].id, count.index)
+    route_table_id = aws_route_table.private.id
+}
+
+## DATABASE ASSOCIATION ##
+resource "aws_route_table_association" "database" {
+    count = length(var.database_subnet_cidrs)
+    subnet_id = element(aws_subnet.database[*].id, count.index)
+    route_table_id = aws_route_table.database.id
+}
